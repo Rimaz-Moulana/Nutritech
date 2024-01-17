a3ab2c7e1cd247587a792665cd9a6f55a08ce19f
Maleesha Manage <87364623+MaleeshaMadhuhansani99@users.noreply.github.com>
2024-01-17 09:05:35 +0530

    annotator side bar

---
+++ frontend/src/components/Dropdown/annotatorvideosdropdown.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Dropdown } from 'flowbite-react';
+
+function annotatorvideosdropdown() {
+
+  return (
+    <div className="flex flex-wrap gap-4 w-7 mr-0">
+    <Dropdown className='mt-0 w-52' placement="right-start">
+        <Dropdown.Item>All Videos</Dropdown.Item>
+        <Dropdown.Item>Annotated Videos</Dropdown.Item>
+        <Dropdown.Item>Unannotated Videos</Dropdown.Item>
+    </Dropdown>
+    </div>
+  )
+}
+
+export default annotatorvideosdropdown
--- frontend/src/components/sidebar/AnnotatorSideBar.jsx
+++ frontend/src/components/sidebar/AnnotatorSideBar.jsx
@@ -5,19 +5,31 @@ import video from '../../assets/Images/video.png';
 import foodproducts from '../../assets/Images/products.png';
 import annotate from '../../assets/Images/edit.png';
 import dropdownIcon from '../../assets/Images/triangledropdown.png';
+// import Dropdown from '../Dropdown/annotatorvideosdropdown'
+import { Dropdown as FlowbiteDropdown } from 'flowbite-react';
+import { useState } from 'react';
 
 
 function AnnotatorSideBar() {
+
+  
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  const handleDropdownToggle = () => {
+    setDropdownOpen(!isDropdownOpen);
+  }; 
   return (
-    <Sidebar className='h-screen bg-sidebarGreen w-24 items-center whitespace-normal px-0 py-0'>
+    <Sidebar className='h-screen bg-sidebarGreen w-32 items-center whitespace-normal px-0 py-0'>
       <Sidebar.Items className="h-screen bg-sidebarGreen px-0 py-0">
         <Sidebar.ItemGroup className='px-0 py-0'>
-          {/* Sidebar items with image icons */}
           <Sidebar.Item href="#"><img src={menu} alt="Menu" className='h-8 w-8 mt-7 mr-7 ml-2 items-center'/></Sidebar.Item>
-          <Sidebar.Item href="#"><img src={home} alt="Menu" className='h-8 w-8 mt-9 mr-7 ml-2 items-center'/></Sidebar.Item>
+          <Sidebar.Item href="#"><img src={home} alt="Menu" className='h-8 w-8 mt-9 mb-9 mr-7 ml-2 items-center'/></Sidebar.Item>
           <div className='relative'>
-            <img src={video} alt="Menu" className='h-8 w-8 mt-9 ml-5 '/>
-            <img src={dropdownIcon} alt="Dropdown" className='absolute right-0 bottom-0 top-2 h-4 w-4'/>
+           <FlowbiteDropdown className='w-52 border-hidden' placement="right-start" label={<img src={video} alt="Menu" className='h-8 w-8 ml-3' />}>
+              <FlowbiteDropdown.Item>All Videos</FlowbiteDropdown.Item>
+              <FlowbiteDropdown.Item>Annotated Videos</FlowbiteDropdown.Item>
+              <FlowbiteDropdown.Item>Unannotated Videos</FlowbiteDropdown.Item>
+            </FlowbiteDropdown>
           </div>
           <Sidebar.Item href="#"><img src={foodproducts} alt="Menu" className='h-8 w-8 mt-10 mr-7 ml-2 items-center'/></Sidebar.Item>
           <Sidebar.Item href="#"><img src={annotate} alt="Menu" className='h-8 w-8 mt-10 mr-7 ml-2 items-center'/></Sidebar.Item>
