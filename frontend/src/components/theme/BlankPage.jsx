19de3cea1da9d379e25475893483a6ada2d6fed6
RimazMoulana <ahmedrimaz99@gmail.com>
2024-01-17 11:58:38 +0530

    commit by rimaz

--- frontend/src/components/theme/BlankPage.jsx
+++ frontend/src/components/theme/BlankPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from '../navbar/Navbar'
-import AnnotatorSideBar from '../sidebar/AnnotatorSideBar'
+// import AnnotatorSideBar from '../sidebar/AnnotatorSideBar'
 
 export default function BlankPage() {
   return (
     <>
     <Navbar />
-    <div className='bg-[#E5EBD6] relative w-full h-auto flex'>
-    <AnnotatorSideBar />  
+    {/* <AnnotatorSideBar />  */}
+    <div className='bg-[#E5EBD6] relative w-full h-full flex'>
+     
         
     </div>
     </>
