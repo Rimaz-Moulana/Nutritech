336e25c08f537dff79a214f41bf15389c6325315
Maleesha Manage <87364623+MaleeshaMadhuhansani99@users.noreply.github.com>
2024-01-19 11:22:56 +0530

    annotation table

--- frontend/src/components/AnnotationTable/TimeStamp.jsx
+++ frontend/src/components/AnnotationTable/TimeStamp.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TimeStamp() {
   return (
-    <div class="relative h-10 w-72 min-w-[200px] mt-64 ml-12">
+    <div class="relative h-10 w-72 min-w-[200px] mt-64 ml-12 mt-44">
       <select
         class="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-gray-900 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50">
         <option >00-05s</option>
