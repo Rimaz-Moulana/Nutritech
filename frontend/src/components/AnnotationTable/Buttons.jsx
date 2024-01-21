d4761da73c1b15af0345cefc0be27501962c2553
Maleesha Manage <87364623+MaleeshaMadhuhansani99@users.noreply.github.com>
2024-01-21 20:25:44 +0530

    navigate to product details from annotation page

--- frontend/src/components/AnnotationTable/Buttons.jsx
+++ frontend/src/components/AnnotationTable/Buttons.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Buttons() {
+  const navigate = useNavigate(); 
+  const viewdetails= () => {
+    navigate('/product');
+  };
   return (
     <div className='flex'>
         <button className="text-white bg-gradient-to-r from-buttonGreen to-darkGreen hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-darkGreen dark:focus:ring-darkGreen shadow-lg shadow-darkGreen dark:shadow-lg dark:shadow-darkGreen font-medium rounded-lg text-sm px-10 py-2.5 text-center me-2 mb-2 ">Save</button>
-        <button className="text-white bg-gradient-to-r from-buttonGreen  to-darkGreen hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-darkGreen dark:focus:ring-darkGreen shadow-lg shadow-darkGreen dark:shadow-lg dark:shadow-darkGreen font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">View Product Details</button>
+        <button className="text-white bg-gradient-to-r from-buttonGreen  to-darkGreen hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-darkGreen dark:focus:ring-darkGreen shadow-lg shadow-darkGreen dark:shadow-lg dark:shadow-darkGreen font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={viewdetails}>View Product Details</button>
     </div>
   )
 }
