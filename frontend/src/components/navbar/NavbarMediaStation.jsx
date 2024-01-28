11b91d4b6efabafb6006e1edc7499ea26bbc8367
Maleesha Manage <87364623+MaleeshaMadhuhansani99@users.noreply.github.com>
2024-01-28 20:38:55 +0530

    media station

--- frontend/src/components/navbar/NavbarMediaStation.jsx
+++ frontend/src/components/navbar/NavbarMediaStation.jsx
@@ -6,7 +6,7 @@ export default function NavbarMediaStation() {
 
   return (
     <div>
-      <nav className="mt-0 mb-6 fixed w-3/4 z-10">
+      <nav className="mt-0 mb-6 fixed w-full z-10">
         <div className="flex p-2 bg-backgroundGreen">
           {/* Logo */}
           <img src={logo} className="h-16 w-44 place-items-start width-full ml-4" alt="NutriTech Logo" />
--- frontend/src/pages/MediaStation/UploadVideo.jsx
+++ frontend/src/pages/MediaStation/UploadVideo.jsx
@@ -7,20 +7,23 @@ import NavbarMediaStation from '../../components/navbar/NavbarMediaStation';
 
 function UploadVideo() {
   return (
-    <div className='bg-backgroundGreen flex'>
-    <div className="w-full ml-12 mr-4 h-full ">
+    <div className='bg-backgroundGreen flex h-screen'>
+    <div className="w-full ml-12 mr-4 ">
       <NavbarMediaStation />
-      <div className='flex h-full justify-between'>
-      <h1 className=' mb-8 mt-32 text-3xl ml-4 font-semibold text-sidebarGreen justify-start h-full '>
+      <div className='flex justify-between'>
+      <h1 className=' mb-8 mt-32 text-3xl ml-4 font-semibold text-sidebarGreen justify-start'>
          Upload Advertisement Video
       </h1>
       
       </div>
-      <div className='flex mt-16 h-full'>
+      <div className='flex mt-16 mr-12'>
       <UploadForm/>
       <VideoUpload/>
       </div>
+      <div className=''>
       <UploadNewVideoUIBtnSet/>
+      </div>
+      
     </div>
   </div>
 );
