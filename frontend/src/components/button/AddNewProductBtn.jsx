db65737dac057573c2b07fdba7f26244dd892640
RimazMoulana <ahmedrimaz99@gmail.com>
2024-02-02 09:19:28 +0530

    commit by rimaz

--- frontend/src/components/button/AddNewProductBtn.jsx
+++ frontend/src/components/button/AddNewProductBtn.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function AddNewProductBtn() {
   const navigate = useNavigate(); 
@@ -7,7 +7,7 @@ export default function AddNewProductBtn() {
     navigate('/product');
   };
   return (
-    <div className='flex mt-28 ml-[450px] bg-'>
+    <div className='flex mt-28 ml-[620px] '>
       <button className="z-10 text-white bg-gradient-to-t from-buttonGreen to-darkGreen hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-darkGreen dark:focus:ring-darkGreen shadow-lg shadow-darkGreen dark:shadow-lg dark:shadow-darkGreen font-medium rounded-lg text-sm px-10 py-2.5 text-center me-2 mb-2 " onClick={addproducts}>
           Add New Product
       </button>
--- frontend/src/components/fields/ProductBar.jsx
+++ frontend/src/components/fields/ProductBar.jsx
@@ -4,9 +4,9 @@ import AllProductBtn from '../button/ProductBarButtonAll'
 
 export default function ProductBar() {
   return (
-    <div className='  w-[80%] lg:ml-60 sm:ml-20 md:ml-[200px] py-2'>
+    <div className='  w-[80%] lg:ml-72 sm:ml-20 md:ml-[200px] py-2'>
         <div className='flex px-2 py-2 text-sidebarGreer border rounded h-[40px] bg-white'>
-         <span className='text-left  font-semibold'>Product</span>
+         <span className='text-left font-bold'>Product</span>
          <div className='inline-flex lg:ml-[708px] md:ml-[200px] sm:ml-[50px]'>
          <AllProductBtn />
          <UpdateProductBtn />
