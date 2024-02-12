71dcd76404dc4388d4e379106a219d66e8571459
MaleeshaMadhuhansani99 <maleeshamapalagama@gmail.com>
2024-02-12 10:35:03 +0530

    changes

--- server/src/routes/videoRoutes.js
+++ server/src/routes/videoRoutes.js
@@ -6,13 +6,17 @@ router.get('/all', videoController.getAllVideos);
 router.get('/annotated-videos', videoController.getAnnotatedVideos);
 router.get('/unannotated-videos', videoController.getUnannotatedVideos);
 router.post('/uploadvideo', videoController.postVideoDetails);
+router.post('/uploadvideo', videoController.postVideoDetails);
 router.get('/history',videoController.getuploadhistory)
 router.get('/sensormanagernewvideo',videoController.getpendingvideos)
 
 
 
 // router.post('/upload',videoController.upload.single('file'), videoController.postVideo);
+// router.post('/upload',videoController.upload.single('file'), videoController.postVideo);
 
+// router.get('/videos', videoController.getVideos);
+// router.post('/addvideo' , videoController.addvideo);
 // router.get('/videos', videoController.getVideos);
 // router.post('/addvideo' , videoController.addvideo);
 
