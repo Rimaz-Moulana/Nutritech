68d58ba60a2da0a604e9b5d2cdf1f861d69b5fbb
MaleeshaMadhuhansani99 <maleeshamapalagama@gmail.com>
2024-05-16 14:07:14 +0530

    reviewhistory

--- frontend/src/pages/ExpertPanel/ReviewHistory.jsx
+++ frontend/src/pages/ExpertPanel/ReviewHistory.jsx
@@ -16,7 +16,7 @@ const [allProducts, setAllProducts] = useState([]);
 const [isLoading, setIsLoading] = useState(false); // Added loading state
 const [error, setError] = useState(null); // Added error state
 const [videoData, setVideoData] = useState([]);
-const [loading,setLoading] =useState(true);
+// const [loading,setLoading] =useState(true);
 //   const [RuleData, setRuleData] = useState([]);
 
 useEffect(() => {
