ecf146f9e54ec39c0f0bcaf4ab028689cddc704d
MaleeshaMadhuhansani99 <maleeshamapalagama@gmail.com>
2024-05-16 12:45:26 +0530

    fix industry reply issue

--- frontend/src/components/CommentSection/ViewComment.jsx
+++ frontend/src/components/CommentSection/ViewComment.jsx
@@ -139,7 +139,7 @@ function ViewComment({ videoId, type }) {
           // Rendering replies
           for (let j = 0; j < Data.reply[i].length; j++) {
               result.push(
-                  <div key={`reply-${i}-${j}`} className='bg-gray-300 text-black mt-4 p-3 text-xl text-left'>
+                  <div key={`reply-${i}-${j}`} className='bg-gray-200 text-black mt-4 p-3 text-xl text-left'>
                       {/* {console.log(Data.reply[i][j].text)} */}
                       <p className='text-gray-600'>{Data.reply[i][j].replyer}</p>
                       <p className='text-gray-600 text-sm'>Reply :</p> 
