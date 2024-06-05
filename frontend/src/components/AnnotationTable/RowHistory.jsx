e0358911481983d4d211ee503d34c914692848c0
MaleeshaMadhuhansani99 <maleeshamapalagama@gmail.com>
2024-06-05 16:57:15 +0530

    decision buttons

--- frontend/src/components/AnnotationTable/RowHistory.jsx
+++ frontend/src/components/AnnotationTable/RowHistory.jsx
@@ -160,6 +160,7 @@ function RowHistory({ videoId, usertype }) {
                     {annotation.finalacceptance[0]?.decision ? (
                       annotation.finalacceptance[0].decision 
                     ): (
+                      (userData.role==="expert head" && (
                       <td className="flex items-end justify-center mt-4 z-10 h-full">
                           <button
                             className='text-white bg-gradient-to-t from-buttonGreen to-darkGreen hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-darkGreen dark:focus:ring-darkGreen shadow-lg shadow-darkGreen dark:shadow-lg dark:shadow-darkGreen font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'
@@ -172,6 +173,7 @@ function RowHistory({ videoId, usertype }) {
                             Disagree
                           </button>
                         </td>
+                    ))
                     )
                     
                        }
